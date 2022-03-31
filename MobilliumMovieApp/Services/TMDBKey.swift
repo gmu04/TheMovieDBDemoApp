208795f51e38a64f2ea8188289890475444a1de1
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-03-31 11:51:00 +0300

    ** Add TMDB.Key struct

---
+++ MobilliumMovieApp/Services/TMDBKey.swift
@@ -0,0 +1,11 @@
+// TMDBKey.swift by Gokhan Mutlu on 31.03.2022
+
+import Foundation
+
+struct TMDB{
+	
+	#error("Please write your own API key for accessing themoviedb.org")
+	static let KEY = "***"
+	
+}
+
