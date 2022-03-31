4b70e23784eccd923b9eb13f86d916cb8bc93de4
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-03-31 14:29:55 +0300

    ** App renamed

---
+++ TheMovieDBDemoApp/Views/ContentView.swift
@@ -0,0 +1,34 @@
+// ContentView.swift by Gokhan Mutlu on 31.03.2022
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	init(){
+		let service = TMDBService(session: URLSession.shared)
+		service.getUpcomingMovies(by: 1) { result in
+			switch result{
+				case .success(let jsonResponse):
+					for movie in jsonResponse.results{
+						print(movie)
+					}
+				case .failure(let apiError):
+					print(apiError.localizedDescription)
+			}
+		}
+		
+	}
+	
+    var body: some View {
+		VStack {
+			Text("Hello, world!")
+				.padding()
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
