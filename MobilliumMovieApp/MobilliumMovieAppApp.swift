7dafeca84096be5b275ab6ec131258d2e85e88b9
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-03-31 09:05:53 +0300

    Initial Commit

---
+++ MobilliumMovieApp/MobilliumMovieAppApp.swift
@@ -0,0 +1,12 @@
+// MobilliumMovieAppApp.swift by Gokhan Mutlu on 31.03.2022
+
+import SwiftUI
+
+@main
+struct MobilliumMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
